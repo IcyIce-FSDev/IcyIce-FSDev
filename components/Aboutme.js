d8282236a68114ef6bdfbd2286626cd179cb1587
IcyIce-FSDev <iceflier95@gmail.com>
2023-09-18 01:13:34 -0700

    fix build issue

--- components/Aboutme.js
+++ components/Aboutme.js
@@ -5,9 +5,9 @@ export default function Aboutme() {
   return (
     <section className={styles.headingMd}>
       <p className={styles.aboutme}>
-        Hi, I'm a FullStack Software Developer and avid outdoor enthusiast. I've
-        been coding for over a year and half. If you want to reach out please
-        use the link(s) below
+        Hi, I am a FullStack Software Developer and avid outdoor enthusiast. I
+        have been coding for over a year and half. If you want to reach out
+        please use the link(s) below
         <br />
         <Link
           href={"https://www.linkedin.com/in/icyice-fsdev/"}
